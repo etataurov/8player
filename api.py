42855c44810c74e3e9dc0c05c7c5237c04516b58
Evgeny Tataurov <tatauroff@gmail.com>
2013-07-15 19:46:35 +0600

    working playing new mix

--- api.py
+++ api.py
@@ -4,7 +4,9 @@ import json
 
 class EightTracksAPI:
     CONFIG_PATH = 'config.json'
+
     def __init__(self):
+        self.play_token = None #maybe also have in config?
         with open(self.CONFIG_PATH) as conf:
             self.config = json.load(conf)
 
@@ -20,6 +22,7 @@ class EightTracksAPI:
 
     @property
     def authenticated(self):
+        # what if we want to logout?
         return 'user_token' in self.config
 
     def _get_mixes(self):
@@ -29,12 +32,54 @@ class EightTracksAPI:
         response_data = json.loads(response.text)
         return response_data['mixes']
 
+    def _get_play_token(self):
+        params = {'user_token': self.config.get('user_token'), 'api_key': self.config.get('api_key')}
+        response = requests.get('{}sets/new.json'.format(self.config.get('service_url')), params=params)
+        response.raise_for_status()
+        response_data = json.loads(response.text)
+        self.play_token = response_data.get('play_token')
+
     def get_mixes(self):
-        return [Mix(x) for x in self._get_mixes()]
+        self._get_play_token()
+        return [Mix(x, self) for x in self._get_mixes()]
+
+    def play_mix(self, mix_id):
+        params = {'user_token': self.config.get('user_token'), 'api_key': self.config.get('api_key'), 'mix_id': mix_id}
+        response = requests.get('{}sets/{}/play.json'.format(self.config.get('service_url'), self.play_token), params=params)
+        response.raise_for_status()
+        response_data = json.loads(response.text)
+        return response_data.get('set').get('track').get('url')
 
 
 class Mix:
-    def __init__(self, params):
+    def __init__(self, params, api):
+        self.api = api
+        self.id = params.get('id')
         self.name = params.get('name')
         self.description = params.get('description')
-        self.id = params.get('id')
+        self.tracks_count = params.get('tracks_count')
+        self.user = params.get('user').get('login')
+
+    def play(self):
+        """
+        Should return just url to first track
+        + some meta-info
+        """
+        return self.api.play_mix(self.id)
+
+
+    def next(self):
+        """
+        same as play
+        maybe dont separate them
+        """
+        pass
+
+    def skip(self):
+        """
+        also checks that skip is possible
+        """
+        pass
+
+    def send_30_sec(self):
+        pass
--- gui.py
+++ gui.py
@@ -1,35 +1,212 @@
 #!/usr/bin/env python3
 
 import sys
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtCore, QtGui
+try:
+    from PyQt4.phonon import Phonon
+except ImportError:
+    app = QtGui.QApplication(sys.argv)
+    QtGui.QMessageBox.critical(None, "Music Player",
+            "Your Qt installation does not have Phonon support.",
+            QtGui.QMessageBox.Ok | QtGui.QMessageBox.Default,
+            QtGui.QMessageBox.NoButton)
+    sys.exit(1)
 import api
 
 
-class MainWindow(QWidget):
+class MainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.tracks_api = api.EightTracksAPI()
         self.dialog = LoginForm(self)
-        self.setWindowTitle("8tracks")
+        #TODO music buttons
+        self.audioOutput = Phonon.AudioOutput(Phonon.MusicCategory, self)
+        self.mediaObject = Phonon.MediaObject(self)
+
+        self.mediaObject.setTickInterval(1000)
+
+        self.mediaObject.tick.connect(self.tick)
+        self.mediaObject.stateChanged.connect(self.stateChanged)
+
+        Phonon.createPath(self.mediaObject, self.audioOutput)
+
+        self.setupActions()
+        self.setupUi()
+
+    def stateChanged(self, newState, oldState):
+        if newState == Phonon.ErrorState:
+            if self.mediaObject.errorType() == Phonon.FatalError:
+                QtGui.QMessageBox.warning(self, "Fatal Error",
+                        self.mediaObject.errorString())
+            else:
+                QtGui.QMessageBox.warning(self, "Error",
+                        self.mediaObject.errorString())
+
+        elif newState == Phonon.PlayingState:
+            self.playAction.setEnabled(False)
+            self.pauseAction.setEnabled(True)
+            self.stopAction.setEnabled(True)
+
+        elif newState == Phonon.StoppedState:
+            self.stopAction.setEnabled(False)
+            self.playAction.setEnabled(True)
+            self.pauseAction.setEnabled(False)
+            self.timeLcd.display("00:00")
+
+        elif newState == Phonon.PausedState:
+            self.pauseAction.setEnabled(False)
+            self.stopAction.setEnabled(True)
+            self.playAction.setEnabled(True)
+
+    def sizeHint(self):
+        return QtCore.QSize(500, 300)
+
+    def setupUi(self):
+        bar = QtGui.QToolBar()
+
+        bar.addAction(self.playAction)
+        bar.addAction(self.pauseAction)
+        bar.addAction(self.stopAction)
+
+        self.seekSlider = Phonon.SeekSlider(self)
+        self.seekSlider.setMediaObject(self.mediaObject)
+
+        self.volumeSlider = Phonon.VolumeSlider(self)
+        self.volumeSlider.setAudioOutput(self.audioOutput)
+        self.volumeSlider.setSizePolicy(QtGui.QSizePolicy.Maximum,
+                                        QtGui.QSizePolicy.Maximum)
+
+        volumeLabel = QtGui.QLabel()
+        volumeLabel.setPixmap(QtGui.QPixmap('images/volume.png'))
+
+        palette = QtGui.QPalette()
+        palette.setBrush(QtGui.QPalette.Light, QtCore.Qt.darkGray)
+
+        self.timeLcd = QtGui.QLCDNumber()
+        self.timeLcd.setPalette(palette)
+
+        headers = ("Title", "Description", "Tracks count", "User")
+
+        self.musicTable = QtGui.QTableWidget(0, 4)
+        self.musicTable.setHorizontalHeaderLabels(headers)
+        self.musicTable.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+        self.musicTable.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
+        self.musicTable.cellPressed.connect(self.tableClicked)
+
+        seekerLayout = QtGui.QHBoxLayout()
+        seekerLayout.addWidget(self.seekSlider)
+        seekerLayout.addWidget(self.timeLcd)
+
+        playbackLayout = QtGui.QHBoxLayout()
+        playbackLayout.addWidget(bar)
+        playbackLayout.addStretch()
+        playbackLayout.addWidget(volumeLabel)
+        playbackLayout.addWidget(self.volumeSlider)
+
+        self.current_track_label = QtGui.QLabel("Nothing")
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.musicTable)
+        mainLayout.addWidget(self.current_track_label)
+        mainLayout.addLayout(seekerLayout)
+        mainLayout.addLayout(playbackLayout)
+
+        widget = QtGui.QWidget()
+        widget.setLayout(mainLayout)
+
+        self.setCentralWidget(widget)
+
+        self.timeLcd.display("00:00")
+        self.setWindowTitle("8tracks music player")
+
+    def tick(self, time):
+        displayTime = QtCore.QTime(0, (time / 60000) % 60, (time / 1000) % 60)
+        self.timeLcd.display(displayTime.toString('mm:ss'))
+
+    def setupActions(self):
+        self.playAction = QtGui.QAction(
+            self.style().standardIcon(QtGui.QStyle.SP_MediaPlay), "Play",
+            self, shortcut="Ctrl+P", enabled=False,
+            triggered=self.mediaObject.play
+        )
+
+        self.pauseAction = QtGui.QAction(
+            self.style().standardIcon(QtGui.QStyle.SP_MediaPause),
+            "Pause", self, shortcut="Ctrl+A", enabled=False,
+            triggered=self.mediaObject.pause
+        )
+
+        self.stopAction = QtGui.QAction(
+            self.style().standardIcon(QtGui.QStyle.SP_MediaStop), "Stop",
+            self, shortcut="Ctrl+S", enabled=False,
+            triggered=self.mediaObject.stop
+        )
+
+        self.nextAction = QtGui.QAction(
+            self.style().standardIcon(QtGui.QStyle.SP_MediaSkipForward),
+            "Next", self, shortcut="Ctrl+N"
+        )
+
+        self.previousAction = QtGui.QAction(
+            self.style().standardIcon(QtGui.QStyle.SP_MediaSkipBackward),
+            "Previous", self, shortcut="Ctrl+R"
+        )
+
+    def addMixToTable(self, mix):
+        titleItem = QtGui.QTableWidgetItem(mix.name)
+        titleItem.setFlags(titleItem.flags() ^ QtCore.Qt.ItemIsEditable)
+
+        descrItem = QtGui.QTableWidgetItem(mix.description)
+        descrItem.setFlags(descrItem.flags() ^ QtCore.Qt.ItemIsEditable)
+
+        countItem = QtGui.QTableWidgetItem(mix.tracks_count)
+        countItem.setFlags(countItem.flags() ^ QtCore.Qt.ItemIsEditable)
+
+        userItem = QtGui.QTableWidgetItem(mix.user)
+        userItem.setFlags(userItem.flags() ^ QtCore.Qt.ItemIsEditable)
+
+        currentRow = self.musicTable.rowCount()
+        self.musicTable.insertRow(currentRow)
+        self.musicTable.setItem(currentRow, 0, titleItem)
+        self.musicTable.setItem(currentRow, 1, descrItem)
+        self.musicTable.setItem(currentRow, 2, countItem)
+        self.musicTable.setItem(currentRow, 3, userItem)
+
+    def tableClicked(self, row, column):
+        source = Phonon.MediaSource(QtCore.QUrl(self.mixes[row].play()))
+        self.mediaObject.setCurrentSource(source)
+        self.mediaObject.play()
+
 
     def check_login(self):
         if not self.tracks_api.authenticated:
             self.dialog.show()
+        else:
+            self.show_mixes()
 
     def authenticate(self, login, password):
-        self.tracks_api.authenticate(login, password)
+        try:
+            self.tracks_api.authenticate(login, password)
+        except Exception:
+            pass
+        else:
+            self.show_mixes()
+
+    def show_mixes(self):
+        self.mixes = self.tracks_api.get_mixes()
+        for mix in self.mixes:
+            self.addMixToTable(mix)
 
 
-class LoginForm(QDialog):
+class LoginForm(QtGui.QDialog):
     def __init__(self, parent=None):
         super(LoginForm, self).__init__(parent)
-        loginlabel = QLabel("Enter login")
-        self.loginedit = QLineEdit()
-        passwordlabel = QLabel("Enter password")
-        self.passwordedit = QLineEdit(echoMode=QLineEdit.Password)
-        self.loginbutton = QPushButton('Login')
-        layout = QVBoxLayout()
+        loginlabel = QtGui.QLabel("Enter login")
+        self.loginedit = QtGui.QLineEdit()
+        passwordlabel = QtGui.QLabel("Enter password")
+        self.passwordedit = QtGui.QLineEdit(echoMode=QtGui.QLineEdit.Password)
+        self.loginbutton = QtGui.QPushButton('Login')
+        layout = QtGui.QVBoxLayout()
         layout.addWidget(loginlabel)
         layout.addWidget(self.loginedit)
         layout.addWidget(passwordlabel)
@@ -37,9 +214,9 @@ class LoginForm(QDialog):
         layout.addWidget(self.loginbutton)
         self.setLayout(layout)
         self.loginedit.setFocus()
-        self.connect(self.loginedit, SIGNAL("returnPressed()"),
+        self.connect(self.loginedit, QtCore.SIGNAL("returnPressed()"),
                      self.login)
-        self.connect(self.passwordedit, SIGNAL("returnPressed()"),
+        self.connect(self.passwordedit, QtCore.SIGNAL("returnPressed()"),
                      self.login)
         self.loginbutton.clicked.connect(self.login)
         self.setWindowTitle("Login")
@@ -51,7 +228,10 @@ class LoginForm(QDialog):
         self.hide()
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    app = QtGui.QApplication(sys.argv)
+
+    app.setApplicationName("8tracks Music Player")
+    app.setQuitOnLastWindowClosed(True)
     window = MainWindow()
     window.show()
     window.check_login()
