d9283f43478b254ee2ab12b394039ba94c058a51
Evgeny Tataurov <tatauroff@gmail.com>
2013-07-17 14:39:08 +0600

    simplified API request

--- api.py
+++ api.py
@@ -13,12 +13,26 @@ class EightTracksAPI:
         with open(self.CONFIG_PATH) as conf:
             self.config = json.load(conf)
 
-    def authenticate(self, login, password):
-        params = {'api_key': self.config.get('api_key')}
-        data = {'login': login, 'password': password}
-        response = requests.post('{}sessions.json'.format(self.config.get('service_url')), data=data, params=params)
+    def _request(self, filename, params=None, method='GET'):
+        if params is None:
+            params = {}
+        url = '{}{}'.format(self.config.get('service_url'), filename)
+        request_params = {'api_key': self.config.get('api_key')}
+        if self.authenticated:
+            request_params['user_token'] = self.config.get('user_token')
+        if method == 'POST':
+            request_dict = dict(params=request_params, data=params)
+        else:
+            request_params.update(params)
+            request_dict = dict(params=request_params)
+        response = requests.request(method, url, **request_dict)
         response.raise_for_status()
         response_data = json.loads(response.text)
+        return response_data
+
+    def authenticate(self, login, password):
+        data = {'login': login, 'password': password}
+        response_data = self._request('sessions.json', data, method='POST')
         self.config['user_token'] = response_data.get('user_token')
         with open(self.CONFIG_PATH, 'w') as conf:
             json.dump(self.config, conf, indent=4)
@@ -30,17 +44,12 @@ class EightTracksAPI:
         return 'user_token' in self.config
 
     def _get_mixes(self):
-        params = {'user_token': self.config.get('user_token'), 'api_key': self.config.get('api_key')}
-        response = requests.get('{}mixes.json'.format(self.config.get('service_url')), params=params)
-        response.raise_for_status()
-        response_data = json.loads(response.text)
+        response_data = self._request('mixes.json')
         return response_data['mixes']
 
     def _get_play_token(self):
-        params = {'user_token': self.config.get('user_token'), 'api_key': self.config.get('api_key')}
-        response = requests.get('{}sets/new.json'.format(self.config.get('service_url')), params=params)
-        response.raise_for_status()
-        response_data = json.loads(response.text)
+        url = self.config.get('user_token')
+        response_data = self._request(url)
         self.play_token = response_data.get('play_token')
 
     def get_mixes(self):
@@ -49,10 +58,8 @@ class EightTracksAPI:
 
     def play_mix(self, mix_id):
         return 'http://127.0.0.1:8888/batman-batman.mp3'
-        params = {'user_token': self.config.get('user_token'), 'api_key': self.config.get('api_key'), 'mix_id': mix_id}
-        response = requests.get('{}sets/{}/play.json'.format(self.config.get('service_url'), self.play_token), params=params)
-        response.raise_for_status()
-        response_data = json.loads(response.text)
+        params = {'mix_id': mix_id}
+        response_data = self._request('sets/{}/play.json'.format(self.play_token), params)
         return response_data.get('set').get('track').get('url')
 
 
